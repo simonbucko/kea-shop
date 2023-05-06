0a5df157645eb5c3a3032f4281d1ddcc56d68a3d
Šimon Bučko <sibu@queue-it.com>
2023-05-06 21:54:28 +0200

    espace delivery address

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,27 +1,29 @@
 import { Router } from "express";
-import Order from "../models/order.js"
+import Order from "../models/order.js";
 import { ORDER_STATUS } from "../constants/constants.js";
 import { sendMail } from "../utils/mailSender/index.js";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-    try {
-        const newOrder = await Order.create({ ...req.body, status: ORDER_STATUS.processing })
-        await sendMail(req.body.email, newOrder._id, req.body.deliveryAddress)
-        res.status(201).json({
-            errors: [],
-            data: null
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({
-            errors: [],
-            data: null
-        })
-    }
-})
+router.post("/", body("deliveryAddress").escape().trim(), async (req, res) => {
+  try {
+    const newOrder = await Order.create({
+      ...req.body,
+      status: ORDER_STATUS.processing,
+    });
+    await sendMail(req.body.email, newOrder._id, req.body.deliveryAddress);
+    res.status(201).json({
+      errors: [],
+      data: null,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      errors: [],
+      data: null,
+    });
+  }
+});
 
-
-
-export default router
+export default router;
