fb0c588c8706d1743955f4ba7b461d97f646a321
Šimon Bučko <sibu@queue-it.com>
2023-05-06 21:45:29 +0200

    escape regex

--- server/routes/productsRouter.js
+++ server/routes/productsRouter.js
@@ -1,78 +1,81 @@
 import { Router } from "express";
-import Product from "../models/product.js"
-import { checkAuth, checkAdmin } from "../middleware/index.js"
+import Product from "../models/product.js";
+import { checkAuth, checkAdmin } from "../middleware/index.js";
+import escapeRegExp from "lodash/escapeRegExp";
 
 const router = Router();
 
 router.post("/", checkAuth, checkAdmin, async (req, res) => {
-    await Product.create(req.body)
+  await Product.create(req.body);
 
-    res.status(201).json({
-        errors: [],
-        data: null
-    })
-})
+  res.status(201).json({
+    errors: [],
+    data: null,
+  });
+});
 
 router.get("/", async (req, res) => {
-    let { page, limit, priceOrder, searchText } = req.query
+  let { page, limit, priceOrder, searchText } = req.query;
 
-    const query = Product.find();
-    if (searchText && searchText !== "") {
-        const regex = new RegExp(searchText);
-        query.where("name").equals({ $regex: regex, $options: "i" })
+  const query = Product.find();
+  if (searchText && searchText !== "") {
+    const regex = new RegExp(escapeRegExp(searchText));
+    query.where("name").equals({ $regex: regex, $options: "i" });
+  }
+  if (priceOrder && priceOrder !== "") {
+    switch (priceOrder) {
+      case "ASC":
+        query.sort({ price: 1 });
+        break;
+      case "DESC":
+        query.sort({ price: -1 });
+        break;
     }
-    if (priceOrder && priceOrder !== "") {
-        switch (priceOrder) {
-            case "ASC":
-                query.sort({ price: 1 })
-                break;
-            case "DESC":
-                query.sort({ price: -1 })
-                break;
-        }
-    }
-    const totalCount = (await query.exec())?.length;
-    const products = await query.skip((page - 1) * limit).limit(limit).clone();
+  }
+  const totalCount = (await query.exec())?.length;
+  const products = await query
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .clone();
 
-
-    res.status(200).json({
-        errors: [],
-        data: {
-            products,
-            totalCount
-        }
-    })
-})
+  res.status(200).json({
+    errors: [],
+    data: {
+      products,
+      totalCount,
+    },
+  });
+});
 
 router.get("/:productId", async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.productId);
-        if (!product) {
-            return res.status(404).json({
-                errors: [
-                    {
-                        msg: "Product not found"
-                    }
-                ],
-                data: null
-            })
-        }
-        res.status(200).json({
-            errors: [],
-            data: {
-                product
-            }
-        })
-    } catch (error) {
-        res.status(404).json({
-            errors: [
-                {
-                    msg: "Product not found"
-                }
-            ],
-            data: null
-        })
+  try {
+    const product = await Product.findById(req.params.productId);
+    if (!product) {
+      return res.status(404).json({
+        errors: [
+          {
+            msg: "Product not found",
+          },
+        ],
+        data: null,
+      });
     }
-})
+    res.status(200).json({
+      errors: [],
+      data: {
+        product,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      errors: [
+        {
+          msg: "Product not found",
+        },
+      ],
+      data: null,
+    });
+  }
+});
 
-export default router
+export default router;
