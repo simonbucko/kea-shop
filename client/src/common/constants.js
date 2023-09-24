5db97db96aeb5a30ae5397747c99d2af5b7dc315
Šimon Bučko <sibu@queue-it.com>
2023-09-24 18:12:55 +0200

    removed build from the ignore

--- client/src/common/constants.js
+++ client/src/common/constants.js
@@ -1,4 +1,7 @@
-export const SERVER_API_URL = "http://localhost:8080/api";
-export const SERVER_SOCKET_URL = "http://localhost:8081/socket";
+// export const SERVER_API_URL = "http://localhost:8080/api";
+export const SERVER_API_URL = "https://kea-foot-shop-api.onrender.com/api";
+// export const SERVER_SOCKET_URL = "http://localhost:8081/socket";
+export const SERVER_SOCKET_URL =
+  "https://kea-foot-shop-socket.onrender.com/socket";
 export const STRIPE_PUBLIC_KEY =
   "pk_test_51IGlnQEa89jh7WASRp8Xjdox1mxCCa6DOScxI9GQZUiuLKDunElBgjxZDyOYfamrM1zYlHxebzk2NJoLkQMhLpnP00Gy73r4Nw";
