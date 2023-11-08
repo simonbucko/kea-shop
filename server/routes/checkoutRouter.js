a71d745523c864e0c1eaaed3bc4977e7f1ad75e6
Šimon Bučko <sibu@queue-it.com>
2023-11-08 21:10:00 +0100

    remove comment

--- server/routes/checkoutRouter.js
+++ server/routes/checkoutRouter.js
@@ -1,23 +1,22 @@
 import { Router } from "express";
-import Stripe from 'stripe'
+import Stripe from "stripe";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-    // initialize Stripe
-    const stripe = new Stripe(process.env.STRIPE_SECRET)
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: req.body.amount,
-        currency: 'eur',
-        payment_method_types: ['card'],
-    })
+  const stripe = new Stripe(process.env.STRIPE_SECRET);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "eur",
+    payment_method_types: ["card"],
+  });
 
-    res.status(200).json({
-        errors: [],
-        data: {
-            clientSecret: paymentIntent.client_secret
-        }
-    })
-})
+  res.status(200).json({
+    errors: [],
+    data: {
+      clientSecret: paymentIntent.client_secret,
+    },
+  });
+});
 
-export default router
+export default router;
